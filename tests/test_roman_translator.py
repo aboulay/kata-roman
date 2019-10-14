56fd1126eefbbd0e3ea8f3f01d34132c67163f99
ADBO <aboulay@octo.com>
2019-10-14 16:07:23 +0200

    add X, V, I*

--- tests/test_roman_translator.py
+++ tests/test_roman_translator.py
@@ -14,5 +14,65 @@ class TestRomanTranslator(unittest.TestCase):
         # Then
         self.assertEqual(result, expected)
 
+    def test_translate_when_asking_for_2(self):
+        # Given
+        given = "2"
+        expected = "II"
+        translator = RomanTranslator()
+        # When
+        result = translator.translate(given)
+        # Then
+        self.assertEqual(result, expected)
+
+    def test_translate_when_asking_for_3(self):
+        # Given
+        given = "3"
+        expected = "III"
+        translator = RomanTranslator()
+        # When
+        result = translator.translate(given)
+        # Then
+        self.assertEqual(result, expected)
+    
+    def test_translate_when_asking_for_5(self):
+        # Given
+        given = "5"
+        expected = "V"
+        translator = RomanTranslator()
+        # When
+        result = translator.translate(given)
+        # Then
+        self.assertEqual(result, expected)
+    
+    def test_translate_when_asking_for_7(self):
+        # Given
+        given = "7"
+        expected = "VII"
+        translator = RomanTranslator()
+        # When
+        result = translator.translate(given)
+        # Then
+        self.assertEqual(result, expected)
+    
+    def test_translate_when_asking_for_10(self):
+        # Given
+        given = "10"
+        expected = "X"
+        translator = RomanTranslator()
+        # When
+        result = translator.translate(given)
+        # Then
+        self.assertEqual(result, expected)
+    
+    def test_translate_when_asking_for_16(self):
+        # Given
+        given = "16"
+        expected = "XVI"
+        translator = RomanTranslator()
+        # When
+        result = translator.translate(given)
+        # Then
+        self.assertEqual(result, expected)
+
 if __name__ == '__main__':
     unittest.main()
--- translator/roman.py
+++ translator/roman.py
@@ -1,3 +1,17 @@
 class RomanTranslator(object):
+
     def translate(self, value):
-      return "I"
+      int_value = int(value)
+      roman_value = ""
+      
+      while int_value != 0:
+        if (int(int_value / 10) > 0):
+          roman_value += "X"
+          int_value -= 10
+        elif (int(int_value / 5) > 0):
+          roman_value += "V"
+          int_value -= 5
+        else:
+          roman_value += "I"
+          int_value -= 1
+      return roman_value
